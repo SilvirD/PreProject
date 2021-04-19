9a8755aa0e083481152677f05293c38d01956f10
SilvirD <bupbi99@gmail.com>
2021-04-19 18:42:11 +0700

    finalll

--- js/detail.js
+++ js/detail.js
@@ -15,6 +15,8 @@ let postButton = document.getElementById("postBtn");
 
 let map = document.getElementById("map");
 
+const starsTotal = 5;
+
 async function getTravelAPI() {
   const travel_response = await fetch("data/travel.json");
   // let travel_response = await fetch("https://sheetdb.io/api/v1/t2jatjycxleq1");
@@ -76,6 +78,12 @@ async function loadData() {
           comment.insertAdjacentHTML(
             "beforeend",
             `<h4>${reviewData[j][`userName`]}</h4>
+            <div class ="${reviewData[j]["userName"]}">
+            <div class="stars-outer">
+            <div class="stars-inner"></div>
+          </div>
+            </div>
+              <span class="number-rating"></span>
           <p>
             <i class="bx bx-time-five"></i>&nbsp; ${reviewData[j]["Date"]}
           </p>
@@ -113,47 +121,89 @@ async function postUserAPI() {
   let params = new URL(document.location).searchParams;
   let postID = getID(params);
 
-  let review = {
-    //Loai request - o day la POST
-    method: "POST",
-    //Thong tin phu chu request
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    //noi dung request
-    body: JSON.stringify({
-      data: [
-        {
-          userID: `${userID}`,
-          postID: `${postID}`,
-          userName: `${userName.value}`,
-          userEmail: `${userEmail.value}`,
-          userComment: `${userComment.value}`,
-          //Rating chua co
-          userRating: 5,
-          Date: `${new Date().getDate()}/${
-            new Date().getMonth() + 1
-          }/${new Date().getFullYear()}`,
-        },
-      ],
-    }),
-  };
-  const res = await fetch(
-    "https://sheetdb.io/api/v1/5012tnw87wtey?fbclid=IwAR0ERbbmyjeuDg6AldYQyak2SL5tI3Bz_4ZIMKgUpDuFaEFkxfDhmfwvifI",
-    review
-  );
-  console.log(await res.json());
+  //Rating
+  for (let i = 0; i < reviewData.length; i++) {
+    let ratings = reviewData[i]["userRating"];
+    console.log(ratings);
+    //Get percentage
+    const starPercentage = (ratings / starsTotal) * 100;
 
-  location.reload();
+    //Round to nearest 10
+    const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+
+    console.log(starPercentageRounded);
+
+    //Set width of stars-inner to percentage
+    document.querySelector(` .stars-inner`).style.width = starPercentageRounded;
+
+    //Add number rating
+    document.querySelector(` .number-rating`).innerHTML = ratings;
+  }
+
+  //Vaidate and Post
+  if (
+    userName.validity.valid === true &&
+    userEmail.validity.valid === true &&
+    userComment.validity.valid === true
+  ) {
+    let review = {
+      //Loai request - o day la POST
+      method: "POST",
+      //Thong tin phu chu request
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      //noi dung request
+      body: JSON.stringify({
+        data: [
+          {
+            userID: `${userID}`,
+            postID: `${postID}`,
+            userName: `${userName.value}`,
+            userEmail: `${userEmail.value}`,
+            userComment: `${userComment.value}`,
+            //Rating chua co
+            userRating: 5,
+            Date: `${new Date().getDate()}/${
+              new Date().getMonth() + 1
+            }/${new Date().getFullYear()}`,
+          },
+        ],
+      }),
+    };
+    const res = await fetch(
+      "https://sheetdb.io/api/v1/5012tnw87wtey?fbclid=IwAR0ERbbmyjeuDg6AldYQyak2SL5tI3Bz_4ZIMKgUpDuFaEFkxfDhmfwvifI",
+      review
+    );
+    console.log(await res.json());
+
+    alert("Thanks for your feedback!");
+
+    location.reload();
+  } else {
+    alert("Please check your infomation again!");
+  }
 }
-
-document.addEventListener("DOMContentLoaded", loadData);
 
 postButton.addEventListener("click", () => {
   postUserAPI();
-  alert("Thanks for your feedback");
 });
+
+// ratingControl.addEventListener("blur", (e) => {
+//   const rating = e.target.value;
+
+//   if (rating > 5) {
+//     alert("Please rate 1 - 5");
+//     return;
+//   }
+
+//   ratings[product] = rating;
+
+//   getRatings();
+// });
+
+document.addEventListener("DOMContentLoaded", loadData);
 
 function getID(params) {
   let ID = params.get("postID");
@@ -163,5 +213,3 @@ function getID(params) {
 function replaceLineBreak(x) {
   return x.replace("\n", "<br>");
 }
-
-console.log(userEmail.oninvalid);
--- js/travel.js
+++ js/travel.js
@@ -7,16 +7,16 @@ let cityName = document.getElementById("cityName");
 
 async function getTravelAPI() {
   let travel_response = await fetch("data/travel.json");
-  console.log(travel_response);
+  // console.log(travel_response);
 
   let travel_data = await travel_response.json();
-  console.log(travel_data);
+  // console.log(travel_data);
 
-  let review_response = await fetch("data/review.json");
-  console.log(review_response);
+  return travel_data;
+}
 
-  let review_data = await review_response.json();
-  console.log(review_data);
+async function locationCity() {
+  let travel_data = await getTravelAPI();
 
   let params = new URL(document.location).searchParams;
   let city = getCity(params);
@@ -30,7 +30,7 @@ async function getTravelAPI() {
         `<div class="location-content col-lg-4">
           <div class="card">
             <img
-              src="https://www.vietfuntravel.com.vn/image/data/Ha-Noi/hoang-thanh-thang-long/Dia-chi-hoang-thanh-thang-long-ha-noi-nam-o-dau-02.jpg"
+              src=${travel_data[i]["location-gallery"]}
               class="img-location"
               alt="location-img"
             />
@@ -50,49 +50,64 @@ async function getTravelAPI() {
       );
     }
   }
-
-  return travel_data;
 }
 
-getTravelAPI();
+async function locationSearch() {
+  let travel_data = await getTravelAPI();
+
+  let resultFound = 0;
+
+  let filter = searchInput.value.toLowerCase();
+
+  if (searchInput.validity.valid === true) {
+    locationPlace.innerHTML = "";
+    for (let i = 0; i < travel_data.length; i++) {
+      if (travel_data[i]["tourist-attraction"].toLowerCase().includes(filter)) {
+        resultFound++;
+        if (resultFound) {
+          locationPlace.insertAdjacentHTML(
+            "beforeend",
+            `<div class="location-content col-lg-4">
+                  <div class="card">
+                    <img
+                      src=${travel_data[i]["location-gallery"]}
+                      class="img-location"
+                      alt="location-img"
+                    />
+                    <div class="card-body">
+                      <h5 class="card-title">${travel_data[i]["tourist-attraction"]}</h5>
+                      <p class="card-intro">
+                        ${travel_data[i]["introduction"]}
+                      </p>
+                      <p>by Duc Tran</p>
+                      <a href="detail.html?postID=${travel_data[i].postID}" class="btn btn-primary">
+                        Detail
+                      </a>
+                      <div class="location-name"></div>
+                    </div>
+                  </div>
+                </div>`
+          );
+        }
+      }
+    }
+    //Show result found
+    document.getElementById(
+      "resultFound"
+    ).innerHTML = `<b>${resultFound}</b> result found`;
+  } else {
+    alert("You must fill the search field");
+    searchInput.focus();
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  locationSearch();
+});
 
 function getCity(params) {
   let city = params.get("city");
   return city;
 }
 
-async function search() {
-  let travel_data = await getTravelAPI();
-
-  let filter = searchInput.value.toLowerCase();
-
-  for (let i = 0; i < travel_data.length; i++) {
-    if (travel_data[i]["post-title"].toLowerCase().indexOf(filter) > -1) {
-      locationPlace.insertAdjacentHTML(
-        "beforeend",
-        `<div class="location-content col-lg-4">
-          <div class="card">
-            <img
-              src="https://www.vietfuntravel.com.vn/image/data/Ha-Noi/hoang-thanh-thang-long/Dia-chi-hoang-thanh-thang-long-ha-noi-nam-o-dau-02.jpg"
-              class="img-location"
-              alt="location-img"
-            />
-            <div class="card-body">
-              <h5 class="card-title">${travel_data[i]["tourist-attraction"]}</h5>
-              <p class="card-intro">
-                ${travel_data[i]["introduction"]}
-              </p>
-              <p>by Duc Tran</p>
-              <a href="detail.html?postID=${travel_data[i].postID}" class="btn btn-primary">
-                Detail
-              </a>
-              <div class="location-name"></div>
-            </div>
-          </div>
-        </div>`
-      );
-    }
-  }
-}
-
-searchBtn.addEventListener("click", search);
+locationCity();
